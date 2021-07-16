a5104cc542f0781e722b3fed8cf151f01af828ee
Mateusz Fibich <mateusz.fibich@gmail.com>
2021-07-16 14:06:19 +0200

    added key id

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,13 +3,15 @@ import "./style.css"
 const Tasks = (props) => (
     <ul className="tasks">
         {props.tasks.map(task => (
-            <li className={`list__item ${task.done && props.hideDoneTasks ? " list__item--hidden" : ""}`}>
+            <li 
+            key={task.id}
+            className={`list__item ${task.done && props.hideDoneTasks ? " list__item--hidden" : ""}`}>
                 <button className="list__button" >
                     {task.done ? "✔" : ""}
                 </button>
-                <span class={`list__taskContent ${task.done ? "list__task--done" : ""}`}>{task.content}
+                <span className={`list__taskContent ${task.done ? "list__task--done" : ""}`}>{task.content}
                 </span>
-                <button class=" list__buttonRemove">🗑</button>
+                <button className=" list__buttonRemove">🗑</button>
             </li>
         ))}
     </ul>
